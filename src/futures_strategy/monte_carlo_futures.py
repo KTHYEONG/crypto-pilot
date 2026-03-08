430bb3c0d91186f69c25f8c72dd58b192727c108
KimTaeHyeong <xogud3624@gmail.com>
2026-03-08 15:11:37 +0900

    chore: remove non-use file(spot) and rename files

--- src/futures_strategy/monte_carlo_futures.py
+++
@@ -1,5 +0,0 @@
-from src.common.monte_carlo import BlockBootstrapMonteCarloSimulator
-
-
-class FuturesMonteCarloSimulator(BlockBootstrapMonteCarloSimulator):
-    pass
--- src/spot_strategy/monte_carlo_spot.py
+++
@@ -1,5 +0,0 @@
-from src.common.monte_carlo import BlockBootstrapMonteCarloSimulator
-
-
-class SpotMonteCarloSimulator(BlockBootstrapMonteCarloSimulator):
-    pass
